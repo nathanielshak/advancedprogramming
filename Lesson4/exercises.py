51899fc3e667cc7d29546c9cef92b48d715a2a45
Martin Michelsen <mjem@wildblue.net>
2019-03-06 20:09:31 -0800

    write tests for lesson 4

--- Lesson4/exercises.py
+++ Lesson4/exercises.py
@@ -42,8 +42,58 @@ def hourglass(size):
 
 
 
-# Everything below here is just to test your code. You don't need to change any
-# of this.
+'''
+The below code is just to test your code. You don't need to touch these.
+'''
 
-if __name__ == '__main__':
-    
+def test_function(fn, tests):
+    print("Testing %s..." % fn.__name__)
+    for i, (args, expected_result) in enumerate(tests):
+        print("Running test %d (%r)..." % (i, args))
+        if not isinstance(args, tuple):
+            args = (args,)
+        actual_result = fn(*args)
+        if actual_result == expected_result:
+            print("PASS!")
+        else:
+            print("FAIL!")
+            print("result was %r, but it should have been %r" % (actual_result, expected_result))
+            return False
+    return True
+
+fn_to_tests = [
+    (square_if_odd, [
+        (0, 0),
+        (1, 1),
+        (2, 2),
+        (3, 9),
+        (-8, -8),
+        (-9, 81),
+    ]),
+    (in_range, [
+        ((0, 0, 0), True),
+        ((1, 3, 2), True),
+        ((-2, 80, 80), True),
+        ((-3, 5, 6), False),
+    ]),
+    (even_elements, [
+        ([1, 2, 5, 7, 2, 6, 7, 9], [2, 2, 6]),
+        ([-8, 2, 4], [-8, 2, 4]),
+        ([1, 3, 5, 7], []),
+    ]),
+    (most_common_letter, [
+        ("abcb", "b"),
+        ("popular", "p"),
+        ("andskfjngrkejnhkjjfjfjnfnkzzjj", "j"),
+    ]),
+    (max_diff, [
+        ([1, 4, -9, 2, 3, 20], 29),
+        ([1, 3, 5], 4),
+        ([-2, 0], 2),
+    ]),
+]
+
+if __name__ == "__main__":
+    for fn, tests in fn_to_tests:
+        test_function(fn, tests)
+        print()
